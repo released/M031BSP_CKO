98eca45a4c10ae4e2d89885f2c144fb79fd5b126
released <released@users.noreply.github.com>
2020-05-05 16:54:52 +0800

    Initial commit

---
+++ SampleCode/Template/main.c
@@ -0,0 +1,172 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * @brief    A project template for M031 MCU.
+ *
+ * Copyright (C) 2017 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+typedef enum{
+	flag_reverse = 0 ,
+	
+	flag_DEFAULT	
+}Flag_Index;
+
+#define CKO_FREQ_24M		(0)
+#define CKO_FREQ_12M		(1)
+#define CKO_FREQ_6M			(2)
+#define CKO_FREQ_3M			(3)
+#define CKO_FREQ_1_5M		(4)
+#define CKO_FREQ_750K		(5)
+#define CKO_FREQ_375K		(6)
+#define CKO_FREQ_187_5K		(7)
+#define CKO_FREQ_93_75K		(8)
+#define CKO_FREQ_46_875K	(9)
+
+uint8_t BitFlag = 0;
+#define BitFlag_ON(flag)							(BitFlag|=flag)
+#define BitFlag_OFF(flag)							(BitFlag&=~flag)
+#define BitFlag_READ(flag)							((BitFlag&flag)?1:0)
+#define ReadBit(bit)								(uint8_t)(1<<bit)
+
+#define is_flag_set(idx)							(BitFlag_READ(ReadBit(idx)))
+#define set_flag(idx,en)							( (en == 1) ? (BitFlag_ON(ReadBit(idx))) : (BitFlag_OFF(ReadBit(idx))))
+
+void GPIO_Init (void)
+{
+    GPIO_SetMode(PB, BIT14, GPIO_MODE_OUTPUT);
+}
+
+void TMR1_IRQHandler(void)
+{
+	static uint32_t LOG = 0;
+	static uint16_t CNT = 0;
+
+    if(TIMER_GetIntFlag(TIMER1) == 1)
+    {
+        TIMER_ClearIntFlag(TIMER1);
+
+		if (CNT++ >= 1000)
+		{		
+			CNT = 0;		
+        	printf("%s : %4d\r\n",__FUNCTION__,LOG++);
+		}
+    }
+}
+
+
+void TIMER1_Init(void)
+{
+    TIMER_Open(TIMER1, TIMER_PERIODIC_MODE, 1000);
+    TIMER_EnableInt(TIMER1);
+    NVIC_EnableIRQ(TMR1_IRQn);	
+    TIMER_Start(TIMER1);
+}
+
+void UART0_Init(void)
+{
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+
+	/* Set UART receive time-out */
+	UART_SetTimeoutCnt(UART0, 20);
+
+	printf("\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHXTFreq : %8d\r\n",CLK_GetHXTFreq());
+	printf("CLK_GetLXTFreq : %8d\r\n",CLK_GetLXTFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());
+	
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC clock (Internal RC 48MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+	
+    /* Wait for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+	
+    /* Disable PLL first to avoid unstable when setting PLL */
+    CLK_DisablePLL();
+
+    /* Set PLL frequency */
+    CLK->PLLCTL = (CLK->PLLCTL & ~(0x000FFFFFul)) | 0x0008C03Eul;
+
+    /* Waiting for PLL ready */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+
+    /* Select HCLK clock source as HIRC and HCLK source divider as 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+	
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV1 | CLK_PCLKDIV_APB1DIV_DIV1);
+
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(TMR1_MODULE);
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_PCLK0, CLK_CLKDIV0_UART0(1));
+    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_HIRC, 0);
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))    |       \
+                    (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB14MFP_Msk)) | SYS_GPB_MFPH_PB14MFP_CLKO;
+	
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*
+ * This is a template project for M031 series MCU. Users could based on this project to create their
+ * own application without worry about the IAR/Keil project settings.
+ *
+ * This template application uses external crystal as HCLK source and configures UART0 to print out
+ * "Hello World", users may need to do extra system configuration based on their system design.
+ */
+
+int main()
+{
+    SYS_Init();
+
+    UART0_Init();
+
+/*
+		0	24000000
+		1	12000000
+		2	6000000
+		3	3000000
+		4	1500000
+		5	750000
+		6	375000
+		7	187500
+		8	93750
+		9	46875
+*/
+
+    CLK_EnableCKO(CLK_CLKSEL1_CLKOSEL_HIRC, CKO_FREQ_6M, 0);
+	
+	TIMER1_Init();
+	
+	
+    /* Got no where to go, just loop forever */
+    while(1)
+    {
+
+    }
+}
+
+/*** (C) COPYRIGHT 2017 Nuvoton Technology Corp. ***/
